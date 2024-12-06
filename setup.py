2df394a8dc14af277fcd8d6aa5ee854683bd7c27
kamleshcode <kamleshptl2003@gmail.com>
2024-12-06 10:42:56 +0530

    structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Kamlesh Patel',
+    author_email= 'kamleshit21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
